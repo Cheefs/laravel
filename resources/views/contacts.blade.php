1f5565456a69d58f2269a52e49532d4b1e465c4e
cheefs <cheefst@outlook.com>
2021-02-19 21:43:02 +0700

    [*] add docker files, add task1 example pages

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'Contacts')
+
+@section('content')
+    <h1>Contacts Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, blanditiis.</p>
+@endsection
---
+++ run.bat
@@ -0,0 +1,1 @@
+docker-compose --env-file ./docker/.env -f ./docker/docker-compose.yaml up -d --build --force-recreate
