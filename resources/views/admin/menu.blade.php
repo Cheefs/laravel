3221fd6edb9948c3b074ab7fdb0cbf14da2b98b3
Cheefs <cheefst@outlook.com>
2021-03-01 19:24:37 +0700

    [*] add assets, move button to admint view

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -4,9 +4,11 @@
         ['route' => 'admin.index', 'name' => __('Admin') ],
         ['route' => 'admin.test1', 'name' => __('Test 1') ],
         ['route' => 'admin.test2', 'name' => __('Test 2') ],
+        ['route' => 'news.create', 'name' => __('Create news') ],
     ];
     if(isset($current) && $current) {
         $links[] = $current;
     }
 @endphp
+
 @include('layouts.menu', ['links' => $links])
