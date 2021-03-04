c65bf4d184da75e36460e2d96271d16bd3c7f5a0
Cheefs <cheefst@outlook.com>
2021-03-04 18:33:09 +0700

    [*] update home view test, add admim view test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -7,14 +7,20 @@ use Tests\TestCase;
 
 class HomeTest extends TestCase
 {
+    const PATH = '/';
 
     public function testHomePageIsLoaded() {
-        $response = $this->get('/');
+        $response = $this->get(self::PATH );
         $response->assertStatus(200);
     }
 
     public function testHomePageHasText() {
-        $response = $this->get('/');
+        $response = $this->get(self::PATH);
         $response->assertSeeText('You are logged in!');
+    }
+
+    public function testHomePageDontHaveAdminMenu() {
+        $response = $this->get(self::PATH);
+        $response->assertDontSee(['Test 1', 'Test 2', 'Create news']);
     }
 }
